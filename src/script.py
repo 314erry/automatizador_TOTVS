044ac672498cc1fc8622983de3e606dc97365a43
Pierry Jonny <108902774+314erry@users.noreply.github.com>
2024-10-30 13:15:59 -0300

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,4 +1,5 @@
 from time import sleep
+from unidecode import unidecode
 import pyautogui
 
 def stringLinha():
@@ -117,7 +118,7 @@ def solicitar():
             sleep(0.7)
             pyautogui.press('down')
             pyautogui.press('enter')
-            pyautogui.write(descSolicit.strip().upper())
+            pyautogui.write(unicode(descSolicit.strip().upper()))
             sleep(0.7)
             pyautogui.press('enter')
             pyautogui.press('down')
